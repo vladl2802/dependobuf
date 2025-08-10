5829ab415a4de5558ffe5b4bf3accdb286eb2523
vladluk <vladl2802@yandex.ru>
2025-08-10 16:26:41 +0300

    add tests on serde

--- dbuf-gen/tests/rust/semantics_tests.rs
+++ dbuf-gen/tests/rust/semantics_tests.rs
@@ -1,3 +1,5 @@
+use std::io::{BufReader, BufWriter};
+
 #[allow(warnings)]
 #[allow(clippy::all)]
 mod basic {
@@ -29,6 +31,25 @@ fn basic_counting() {
     assert_eq!(four1, four2);
 }
 
+#[test]
+fn basic_serde() {
+    let zero = basic::Nat::zero().expect("couldn't construct zero");
+    let one = basic::Nat::suc(Box::new(zero)).expect("couldn't construct one");
+
+    let mut writer = BufWriter::new(Vec::new());
+    one.clone()
+        .serialize(&mut writer)
+        .expect("couldn't serialize to writer");
+
+    let buffer = writer.into_inner().expect("couldn't retrieve buffer");
+
+    let mut reader = BufReader::new(buffer.as_slice());
+    let one_new = basic::Nat::deserialize(basic::nat::Dependencies {}, &mut reader)
+        .expect("couldn't deserialize");
+
+    assert_eq!(one, one_new);
+}
+
 #[allow(warnings)]
 #[allow(clippy::all)]
 mod nat_vec {
@@ -54,4 +75,33 @@ fn nat_vec_extending() {
         )
         .expect("couldn't construct Following");
     }
+}
+
+#[test]
+fn nat_vec_serde() {
+    let len = nat_vec::Nat::suc(Box::new(
+        nat_vec::Nat::zero().expect("couldn't construct zero"),
+    ))
+    .expect("couldn't construct one");
+
+    let vec = nat_vec::Vec::cons(
+        Box::new(nat_vec::Nat::zero().expect("couldn't construct zero")),
+        Box::new(nat_vec::Nat::zero().expect("couldn't construct zero")),
+        Box::new(nat_vec::Vec::nil().expect("couldn't construct nil")),
+    )
+    .expect("couldn't construct cons");
+
+    let mut writer = BufWriter::new(Vec::new());
+    vec.clone()
+        .serialize(&mut writer)
+        .expect("couldn't serialize to writer");
+
+    let buffer = writer.into_inner().expect("couldn't retrieve buffer");
+
+    let mut reader: BufReader<&[u8]> = BufReader::new(buffer.as_slice());
+    let vec_new =
+        nat_vec::Vec::deserialize(nat_vec::vec::Dependencies { n: Box::new(len) }, &mut reader)
+            .expect("couldn't deserialize");
+
+    assert_eq!(vec, vec_new);
 }
